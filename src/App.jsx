3dd12ad8c5a12da10f4a7f2c96a4bc7043d9a590
bhagvan singh lodhi <bhagvansinghhere@gmail.com>
2025-10-03 22:01:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,56 @@ import MockTests from './pages/MockTests'
 import Resources from './pages/Resources'
 import Profile from './pages/Profile'
 import Login from './pages/Login' 
-import Register from './pages/Register';
+import Register from './pages/Register'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
+  console.log('App rendered - checking routes');
+  
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
           <Routes>
+            {/* Public routes */}
             <Route path="/" element={<Home />} />
-            <Route path="/practice" element={<Practice />} />
-            <Route path="/mock-tests" element={<MockTests />} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/profile" element={<Profile />} />
             <Route path="/login" element={<Login />} />
-               <Route path="/register" element={<Register />} />
+            <Route path="/register" element={<Register />} />
+            
+            {/* Protected routes */}
+            <Route 
+              path="/practice" 
+              element={
+                <ProtectedRoute>
+                  <Practice />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/mock-tests" 
+              element={
+                <ProtectedRoute>
+                  <MockTests />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/resources" 
+              element={
+                <ProtectedRoute>
+                  <Resources />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/profile" 
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              } 
+            />
           </Routes>
         </main>
         <Footer />
